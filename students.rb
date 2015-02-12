430d9ded41dbe9fd599c4072662673e81ad90af4
Eleanor Reid <elereid@gmail.com>
2015-02-12 11:14:17 -0500

    final change

--- students.rb
+++ students.rb
@@ -1,9 +1,33 @@
-students=["tom","carol", "john","susan","sam","harry","betty","mark","mary","ned","henry","paul"]
-# students.shuffle!
-# puts students
-
-count=0
-until count == students.length
-	puts "#{students[count]} & #{students[count+1]}"
-	count += 2
+# students=["tom","carol", "john","susan","sam","harry","betty","mary","ned","henry","paul"]
+# # students.shuffle!
+# # puts students
+students=[]
+name=""
+puts "Name everyone who showed up to school today, then enter 'done'"
+while name != "done" 
+		name =gets.chomp
+		students.push(name)
 end
+		students.pop
+count=0
+num_students =students.length
+until count == num_students
+if num_students==1
+	puts "Sorry you're the only one today.  But we'll have fun."
+	break
+else
+	if num_students % 2==1
+		if count<num_students-3
+		puts "#{students[count]} & #{students[count+1]}"
+		else
+		puts "#{students[count]}, #{students[count+1]}, & #{students[count+2]}" 
+		break
+		end
+	else
+		puts "#{students[count]} & #{students[count+1]}"
+	end
+	count +=2
+end
+end
+
+
