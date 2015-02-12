ba1b78e5191c866f982257fb5990b1c2fa88e6b8
Eleanor Reid <elereid@gmail.com>
2015-02-12 09:48:09 -0500

    paired students

--- students.rb
+++ students.rb
@@ -1,1 +1,9 @@
-students=["tom","carol", "john","susan","sam","harry","betty","mary","ned","henry","paul"]
+students=["tom","carol", "john","susan","sam","harry","betty","mark","mary","ned","henry","paul"]
+# students.shuffle!
+# puts students
+
+count=0
+until count == students.length
+	puts "#{students[count]} & #{students[count+1]}"
+	count += 2
+end
